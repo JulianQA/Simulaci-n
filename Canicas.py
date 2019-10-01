3c79aa63c15e69b8ac295adb159615408935fe94
JulianQA <54037880+JulianQA@users.noreply.github.com>
2019-10-01 15:45:27 -0500

    Update Canicas.py

--- Canicas.py
+++ Canicas.py
@@ -7,34 +7,47 @@ def multi(m1,m2):
                 matriz_res[i][j] += m1[i][k]*m2[k][j]
     return matriz_res
 
-def canicas1():
-    bol = [[0]*6 for i in range (6)]
-    bol [2][1] = 1
-    bol [3][3] = 1
-    bol [2][5] = 1
-    bol [4][2] = 1
-    bol [5][0] = 1
-    bol [5][4] = 1
-    cantidad = [[6],[2],[1],[5],[3],[10]]
-    click = 1
-    res = multi(bol,bol)
-    click -= 1
-    while click>0:
-        res = multi(res,bol)
-        click -= 1
-    
-    return multi(res,cantidad)
+def suma(a,b):
+    ent = a[0]+b[0]
+    imag = a[1]+b[1]
+    return ent,imag
 
-def vec(v,m):
-    res = [[0]*len(m[0]) for i in range(len(m))]
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            res[i][j] = round(v * m[i][j],2)
-    return res
-def multiple(m1,m2):
-    res = []
+def multiplicacion(a,b):  
+    first = a[0] * b[0]
+    second = a[0] * b[1]
+    third = a[1] * b[0]
+    fourth = a[1] * b[1]
+    ent = first-fourth
+    imag = second+third
+    return ent,imag
+
+def multiplicacion_matrices(m1,m2):
+    matriz_res = [[(0,0)]*len(m2[0]) for i in range(len(m1))]
+    cos = 0
+    if len(matriz_res) == 1:
+        cos = 1
+        
     for i in range(len(m1)):
-        for j in range(len(m1[0])):
-            res.append(vec(m1[i][j],m2))
-    return res
+        for j in range(len(m2[0])):
+            for k in range(len(matriz_res)+cos):
+                matriz_res[i][j] = suma(matriz_res[i][j],multiplicacion(m1[i][k],m2[k][j]))
+    return matriz_res
+
+def canicas1(m1,v1,clicks):
+
+    res = multi(m1,m1)
+    clicks -= 1
+    while clicks > 0:
+        res = multi(res,m1)
+        clicks -= 1
+    
+    return multi(res,v1)
+
+def canicasimag(m1,v1,c):
+    res = multiplicacion_matrices(m1,m1)
+    clicks -= 1
+    while clicks > 0:
+        res = multiplicacion_matrices(res,m1)
+        clicks -= 1
+    return multiplicacion_matrices(res,v1)
          
